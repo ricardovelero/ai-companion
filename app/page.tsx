0b6fec4525f5faaf72cd425a06d029b76be3cbba
Ricardo Rodriguez <ricardovelero@gmail.com>
2023-08-21 08:21:40 +0200

    Folders setup

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <div>
-      <div className="text-green-500 text-3xl">Hello AI Companion</div>
-    </div>
-  );
-}
