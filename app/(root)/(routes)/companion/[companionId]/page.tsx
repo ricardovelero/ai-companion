85fcd12d459f5b99a0087886b79e8fd611768abc
Ricardo Rodriguez <ricardovelero@gmail.com>
2023-08-22 11:55:06 +0200

    Fix security hole with user id

--- app/(root)/(routes)/companion/[companionId]/page.tsx
+++ app/(root)/(routes)/companion/[companionId]/page.tsx
@@ -1,5 +1,6 @@
 import prismadb from "@/lib/prismadb";
 import CompanionForm from "./components/companion-form";
+import { auth, redirectToSignIn } from "@clerk/nextjs";
 
 interface CompanionIdPageProps {
   params: {
@@ -10,10 +11,14 @@ interface CompanionIdPageProps {
 export default async function CompanionIdPage({
   params,
 }: CompanionIdPageProps) {
+  const { userId } = auth();
   //TODO: Check subscription
+
+  if (!userId) return redirectToSignIn();
 
   const companion = await prismadb.companion.findUnique({
     where: {
+      userId,
       id: params.companionId,
     },
   });
