d6eef87f24dfb7e95053affe95b100c72cf323c8
Ricardo Rodriguez <ricardovelero@gmail.com>
2023-08-23 17:47:26 +0200

    Protect Create in mobile too

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -2,14 +2,18 @@ import { Menu } from "lucide-react";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import Sidebar from "@/components/sidebar";
 
-export default function MobileSidebar() {
+interface SidebarProps {
+  isPro: boolean;
+}
+
+export default function MobileSidebar({ isPro }: SidebarProps) {
   return (
     <Sheet>
       <SheetTrigger className="md:hidden pr-4">
         <Menu />
       </SheetTrigger>
       <SheetContent side="left" className="p-0 bg-secondary pt-10 w-32">
-        <Sidebar isPro={false} />
+        <Sidebar isPro={isPro} />
       </SheetContent>
     </Sheet>
   );
